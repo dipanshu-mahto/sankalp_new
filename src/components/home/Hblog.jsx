5d0d30a78d91313db8b9dd4703b61c6f097e0581
Sneh Prabha Raj <96184885+snehpr966@users.noreply.github.com>
2024-01-07 00:04:45 +0530

    Update Hblog.jsx

--- src/components/home/Hblog.jsx
+++ src/components/home/Hblog.jsx
@@ -15,22 +15,11 @@ const Hblog = () => {
             {blog.slice(0, 3).map((val) => (
               <div key={val.id} className='items shadow'>
                 <div className='img'>
-                  <img src={val.cover} alt='' />
+                
                 </div>
                 <div className='text'>
                   <div className='admin flexSB'>
-                    <span>
-                      <i className='fa fa-user'></i>
-                      <label htmlFor=''>{val.type}</label>
-                    </span>
-                    <span>
-                      <i className='fa fa-calendar-alt'></i>
-                      <label htmlFor=''>{val.date}</label>
-                    </span>
-                    <span>
-                      <i className='fa fa-comments'></i>
-                      <label htmlFor=''>{val.com}</label>
-                    </span>
+                  
                   </div>
                   <h1>{val.title}</h1>
                   <p>{val.desc}</p>

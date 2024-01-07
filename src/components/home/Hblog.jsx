818a74f6cad6bfc6028e977a67b21c4b96b58173
Sneh Prabha Raj <96184885+snehpr966@users.noreply.github.com>
2024-01-07 23:34:51 +0530

    Update Hblog.jsx

--- src/components/home/Hblog.jsx
+++ src/components/home/Hblog.jsx
@@ -10,12 +10,12 @@ const Hblog = () => {
     <>
       <section className='blog'>
         <div className='container'>
-          <Heading subtitle='Notices' title='Recent From Notices' />
+        <a href="https://drive.google.com/folderview?id=1RmsOC2D4uQANwg0VStFamy4e7hhuKqwb"><Heading subtitle='Notices' title='Recent From Notices' /></a>
           <div className='grid2'>
             {blog.slice(0, 3).map((val) => (
               <div key={val.id} className='items shadow'>
                 <div className='img'>
-                
+                  {/* <a href="https://youtube.com/@sankalp-ghargharshikshakaa6348?si=pSuJXSmLXy9ToTUP"></a> */}
                 </div>
                 <div className='text'>
                   <div className='admin flexSB'>
